cbf61359f9e647abddc95eb353db56fb2cbe045d
PebblesProgramming <milan0709@hotmail.nl>
2025-05-19 22:48:27 +0200

    Some playing around and testing with Ashley

--- core/src/main/java/com/milan/warfare/Main.java
+++ core/src/main/java/com/milan/warfare/Main.java
@@ -1,34 +1,45 @@
 package com.milan.warfare;
-
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.milan.warfare.components.PositionComponent;
+import com.milan.warfare.components.VelocityComponent;
+import com.milan.warfare.systems.MovementSystem;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+   
+   private Engine engine;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        engine = new Engine();
+
+        engine.addSystem(new MovementSystem());
+
+        Entity player = new Entity();
+        PositionComponent position = new PositionComponent();
+        position.x = 0;
+        position.y = 0;
+
+        VelocityComponent velocity = new VelocityComponent();
+        velocity.x = 50;
+        velocity.y = 0;
+
+        player.add(position);
+        player.add(velocity);
+
+        engine.addEntity(player);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        float delta = Gdx.graphics.getDeltaTime();
+        engine.update(delta);
     }
 
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
 }
